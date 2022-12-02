c435c91ed197a044e7d022cc2a0983740ea4cced
patteesolomon <patteesolomon@gmail.com>
2022-12-01 22:57:30 -0600

    ef

--- server.js
+++ server.js
@@ -5,21 +5,36 @@ const port = 3000;
 var beer = 99;
 var bugs = null;
 var randP = null;
-var message = `- On the home page (get "/"), users should see:
-- "99 Bottles of beer on the wall"
-- a link that says "take one down, pass it around"
-- this should link to /98, where the number represents the number of bottles left.
-- When a number is given in the url (get "/:number_of_bottles"), users should see:
-- The number of bottles of beer on the wall (i.e. 98 Bottles of beer on the wall.)
-- a link to "take one down, pass it around", where the href is number of bottles in the parameter minus 1.
-- If there are 0 bottles left, do not show a link to "take one down"
-- Add a link to start over, which directs the user back to the home page.`;
+var lives = 7;
+
+var linker = `<a href = '/:number_of_bottles'> "take one down pass it around" </a>`;
+
 app.get('/' , (req, res) =>{
-    res.redirect();
+    res.send(
+    `${beer} Bottles of beer on the wall ` + '\n' 
+    + `${linker}` + '\n' 
+    + `\n ${lives} Number of my lives wasted.`
+    );
 });
 
-app.get('/', (req, res) =>{
-
+app.get('/:number_of_bottles', (req, res) => {
+    --beer; // takes 2 
+    if (beer == 0 || lives == 0) {
+        res.send(
+            `${beer} Bottles of beer on the wall ` + '\n' 
+            + `` + '\n' 
+            + `${lives} Number of my lives wasted.`
+        );
+    }
+    else{
+        res.send(
+            `${beer} Bottles of beer on the wall ` + '\n' 
+            + `${linker}` + '\n' 
+            + `${lives} Number of my lives wasted.`
+        );
+    }
 });
 
-app.get
+app.listen(port, () => {
+    console.log('listening');
+});
